be8ca47a473d74d3c12a3d76cdb69a2315787d51
Daniel <darc7zed@gmail.com>
2024-08-19 23:15:43 -0400

    Added winner state, func checkWinner. Created updateBoard func

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -5,11 +5,22 @@ const TURNS = {
 	O: 'o'
 }
 
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
 const Square = ({children, isSelected, updateBoard, index}) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`
 
   const handleClick = ()=>{
-    updateBoard()
+    updateBoard(index)
   }
 
   return (
@@ -18,12 +29,36 @@ const Square = ({children, isSelected, updateBoard, index}) => {
     </div>
   )
 }
+
 function App(){
-  const [board, setBoard] = useState(Array(9).fill(null))
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+  
+  const [winner, setWinner] = useState(null);
+  
+  const checkWinner = (boardToCheck)=>{
+    for (const combo of WINNER_COMBOS){
+      const [a,b,c] = combo;
+      if (boardToCheck[a] && 
+          boardToCheck[a] === boardToCheck[b] &&
+          boardToCheck[a] === boardToCheck[c]
+      ) {
+          return boardToCheck[a]
+      }
+      //no winner
+      return null;
+    }
+  }
 
-  const [turn, setTurn] = useState(TURNS.X)
-
-  const updateBoard = ()=>{
+  const updateBoard = (index)=>{
+    if (board[index]) return
+    //new board
+    const newBoard = [...board];
+    newBoard[index]=turn;
+    setBoard(newBoard);
+    //new turn
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
   }
 
 	return(
@@ -38,7 +73,7 @@ function App(){
                   index={index}
                   updateBoard={updateBoard}
                 >
-                  {index}
+                  {board[index]}
                 </Square>
               )
             })
