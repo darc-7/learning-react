2b7016b77dd1fee7a894025774b7ba40a89920bb
DARC <darc7zed@gmail.com>
2024-09-17 19:34:06 -0400

    Extract to components

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import confetti from "canvas-confetti"
 
 import { Square } from "./components/Square"
+import { Board } from "./components/Board"
 import { TURNS } from "./constants"
 import { checkWinner, checkEndGame } from "./logic/board"
 import { WinnerModal } from "./components/WinnerModal"
@@ -39,28 +40,16 @@ function App(){
 		<main className="board">
 			<h1>Tic-tac-toe</h1>
       <button onClick={resetGame}>Restart</button>
-			  <section className="game">
-          {
-            board.map((square,index)=>{
-              return(
-                <Square 
-                  key={index}
-                  index={index}
-                  updateBoard={updateBoard}
-                >
-                  {square}
-                </Square>
-              )
-            })
-          }
-			  </section>
+      <section className="game">
+        {<Board board={board} updateBoard={updateBoard}/>}
+      </section>
 
-        <section className="turn">
-          <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
-          <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
-        </section>
-      <WinnerModal resetGame={resetGame} winner={winner}/>
-		</main>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+    <WinnerModal resetGame={resetGame} winner={winner}/>
+  </main>
 	)
 }
 
