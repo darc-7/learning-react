b3a1b201743208830a2e4bdc3c9d4b01134eafef
DARC <darc7zed@gmail.com>
2024-12-04 21:43:44 -0400

    3rd project complete

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,7 +1,56 @@
-function App() {
+import {useEffect, useState} from "react"
+//useEffects can be used to connect to 3rd party APIs. Data fetching, tracking, etc. 
+
+const FollowMouse = () =>{
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({x: 0, y: 0})
+  //initialize a State with the correct type data is good practice
+
+  useEffect(()=>{
+    console.log("efecto", {enabled})
+
+    const handleMove = (event)=>{
+      const {clientX, clientY} = event
+      console.log({clientX, clientY})
+      setPosition({x: clientX, y: clientY})
+    }
+    if(enabled){
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
 
   return (
-  <h1>Proyecto 3</h1>
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(255, 0, 255, 0.6)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      {/* ternary operator */}
+      <button onClick={()=>setEnabled(!enabled)}>
+        {enabled ? 'Desactivar':'Activar'} efecto
+      </button>
+    </>
+  )
+}
+function App() {
+  return (
+    <main>
+      <FollowMouse/>
+    </main>
   )
 }
 
